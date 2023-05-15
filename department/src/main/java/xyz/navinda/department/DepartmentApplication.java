b49130f3162d252cd3914c8f65d6022421cb42e0
ipmanlk <io@navinda.xyz>
2023-05-15 20:19:53 +0530

    feat: discovery server and discovery client

--- department/src/main/java/xyz/navinda/department/DepartmentApplication.java
+++ department/src/main/java/xyz/navinda/department/DepartmentApplication.java
@@ -2,10 +2,11 @@ package xyz.navinda.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DepartmentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentApplication.class, args);
 	}
